f3b665a8a50ea89cfe0c4eb88c3ccf6d0de705b4
Minna Niemi <minna.niemi@futurice.com>
2018-12-01 16:14:34 +0000

    Go down the very questionable exception route

--- src/day1.test.ts
+++ src/day1.test.ts
@@ -1,16 +1,34 @@
-import { addNumbersInArray } from "./day1";
+import {
+  addNumbersInArray,
+  reduceWithStartingValue,
+  adderThatThrowsException
+} from "./day1";
 import { frequencyChanges } from "./inputs/day1";
 
 describe("addNumbersInArray", () => {
   test("It should add numbers in an array together", () => {
     const testArray = [+1, -2, +3, +1];
-    expect(addNumbersInArray(testArray)).toEqual(3);
+    expect(addNumbersInArray(testArray, 0)).toEqual(3);
   });
 
   test("It should add these numbers together too", () => {
     const testArray = [-1, -2, -3];
-    expect(addNumbersInArray(testArray)).toEqual(-6);
+    expect(addNumbersInArray(testArray, 0)).toEqual(-6);
   });
 
-  console.log("The answer is: ", addNumbersInArray(frequencyChanges));
+  test("It should add these numbers, given a non-zero starting value", () => {
+    const testArray = [-1, -2, -3];
+    expect(addNumbersInArray(testArray, 7)).toEqual(1);
+  });
+
+  // console.log("The answer is: ", addNumbersInArray(frequencyChanges, 0));
+});
+
+describe("exceptionThrowingReducer", () => {
+  test("It throws an exception if you use it", () => {
+    const testArray = [2, 3, -6, 7];
+    expect(() =>
+      reduceWithStartingValue(testArray, 0, adderThatThrowsException(5))
+    ).toThrow();
+  });
 });
--- src/day1.ts
+++ src/day1.ts
@@ -1,2 +1,22 @@
-export const addNumbersInArray = (numbers: number[]) =>
-  numbers.reduce((acc, value) => acc + value);
+export const adderThatThrowsException = (throwAt: number) => (
+  acc: number,
+  value: number
+) => {
+  const resultingValue = acc + value;
+  if (resultingValue === throwAt) {
+    throw resultingValue;
+  }
+  return resultingValue;
+};
+
+export const reduceWithStartingValue = (
+  numbers: number[],
+  startingValue: number,
+  reducer: (acc: number, value: number) => number
+) => startingValue + numbers.reduce(reducer);
+
+export const addNumbersInArray = (numbers: number[], startingValue: number) =>
+  reduceWithStartingValue(numbers, startingValue, simpleNumberAdder);
+
+const simpleNumberAdder = (number1: number, number2: number) =>
+  number1 + number2;
